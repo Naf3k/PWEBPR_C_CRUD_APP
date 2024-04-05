924d312153a9279ba7ef7689630f945bddc3cb41
Ahmad Nafi <133883341+Naf3k@users.noreply.github.com>
2024-04-05 20:52:35 +0700

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dashboard.css">
+    <title></title>
+</head>
+<body>
+    <table>
+        <thead>
+          <tr>
+            <th>Option</th>
+            <th>Default</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+  </div>
+    <div class="sidenav">
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+    <div>
+    <div class = "logout"><button>logout</button></div>
+</body>
+</html>
